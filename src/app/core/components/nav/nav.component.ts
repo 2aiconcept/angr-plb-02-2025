bb492b5fc6dcfc2f19ac30b65faa4e3a24659688
2aiconcept <contact@2aiconcept.com>
2025-02-20 14:50:47 +0100

    @Input() and NgClass on nav component

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IconDashbordComponent } from '../../../icons/components/icon-dashbord/icon-dashbord.component';
 import { IconOrdersComponent } from '../../../icons/components/icon-orders/icon-orders.component';
 import { IconProductsComponent } from '../../../icons/components/icon-products/icon-products.component';
 import { IconCustomersComponent } from '../../../icons/components/icon-customers/icon-customers.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -11,8 +12,11 @@ import { IconCustomersComponent } from '../../../icons/components/icon-customers
     IconOrdersComponent,
     IconProductsComponent,
     IconCustomersComponent,
+    NgClass,
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
-export class NavComponent {}
+export class NavComponent {
+  @Input() isUiSidenav!: boolean;
+}

43565e17c3613980875d00a4c24b991c4dc83d20
2aiconcept <contact@2aiconcept.com>
2025-02-21 18:03:36 +0100

    get set collection and httpClient OrdersService

--- src/app/features/orders/services/orders.service.ts
+++ src/app/features/orders/services/orders.service.ts
@@ -1,18 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Order } from '../models/order';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
-  pCollection!: Observable<Order[]>;
+  private pCollection!: Observable<Order[]>;
+  private url = environment.urlApi;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.collection = this.http.get<Order[]>(`${this.url}/orders`);
+  }
 
   // get collection
+  get collection(): Observable<Order[]> {
+    return this.pCollection;
+  }
 
   // set collection
+  set collection(col: Observable<Order[]>) {
+    this.pCollection = col;
+  }
 
   // change state order
 

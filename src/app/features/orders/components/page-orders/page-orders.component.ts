09ddc46bc865ba46abe1e780dc3c0f6b9128b7cc
2aiconcept <contact@2aiconcept.com>
2025-02-21 18:26:33 +0100

    page orders component ts and html

--- src/app/features/orders/components/page-orders/page-orders.component.ts
+++ src/app/features/orders/components/page-orders/page-orders.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OrdersService } from '../../services/orders.service';
+import { IconDeleteComponent } from '../../../../icons/components/icon-delete/icon-delete.component';
+import { IconEditComponent } from '../../../../icons/components/icon-edit/icon-edit.component';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { StateOrder } from '../../enums/state-order';
 
 @Component({
   selector: 'app-page-orders',
-  imports: [],
+  imports: [
+    IconDeleteComponent,
+    IconEditComponent,
+    NgIf,
+    NgFor,
+    FormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './page-orders.component.html',
   styleUrl: './page-orders.component.scss',
 })
-export class PageOrdersComponent {}
+export class PageOrdersComponent {
+  private ordersService = inject(OrdersService);
+  collection$ = this.ordersService.collection;
+  states = Object.values(StateOrder);
+}
